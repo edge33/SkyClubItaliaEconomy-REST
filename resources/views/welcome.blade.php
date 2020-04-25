040638fd03f2563f8344e23251c2c11dfaae2d9a
Francesco <francescomaida91@gmail.com>
2020-04-25 15:15:18 +0200

    final fixes for initial release

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SkyClubItalia <i>economy</i>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="m-b-md">
+                    work in progress...
                 </div>
             </div>
         </div>
